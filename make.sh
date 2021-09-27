5ba7366cbdb15df6f42c829e14587b16230b7665
Jonny Burger <jonathanburger11@gmail.com>
2021-09-27 16:35:42 +0200

    upload script

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-cd chrome && zip -9 --filesync --recurse-paths chrome.zip .
+cd chromium && zip -9 --filesync --recurse-paths chromium.zip .
 cd ..
 cd ffmpeg && zip -9 --filesync --recurse-paths ffmpeg.zip .
 cd ..
@@ -6,6 +6,6 @@ cd remotion && zip -9 --filesync --recurse-paths remotion.zip .
 cd ..
 
 mkdir -p out
-mv chrome/chrome.zip out/remotion-layer-chrome-v1.zip
+mv chromium/chromium.zip out/remotion-layer-chromium-v1.zip
 mv ffmpeg/ffmpeg.zip out/remotion-layer-ffmpeg-v1.zip
 mv remotion/remotion.zip out/remotion-layer-remotion-v1.zip
---
+++ upload.sh
@@ -0,0 +1,3 @@
+aws s3 cp out/remotion-layer-chromium-v1.zip s3://lambda-remotion-binaries-eu-central-1/remotion-layer-chromium-v1.zip
+aws s3 cp out/remotion-layer-ffmpeg-v1.zip s3://lambda-remotion-binaries-eu-central-1/remotion-layer-ffmpeg-v1.zip
+aws s3 cp out/remotion-layer-remotion-v1.zip s3://lambda-remotion-binaries-eu-central-1/remotion-layer-remotion-v1.zip
