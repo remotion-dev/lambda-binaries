dbc7db8bd538b1ac8e01c2803ccda6484d80dc5a
Jonny Burger <jonathanburger11@gmail.com>
2021-11-19 21:24:59 +0100

    remove unnecessary

--- make.sh
+++ make.sh
@@ -6,6 +6,6 @@ cd remotion && zip -9 --filesync --recurse-paths remotion.zip .
 cd ..
 
 mkdir -p out
-mv chromium/chromium.zip out/remotion-layer-chromium-v1.zip
-mv ffmpeg/ffmpeg.zip out/remotion-layer-ffmpeg-v1.zip
-mv remotion/remotion.zip out/remotion-layer-remotion-v1.zip
+mv chromium/chromium.zip out/remotion-layer-chromium-v2.zip
+mv ffmpeg/ffmpeg.zip out/remotion-layer-ffmpeg-v2.zip
+mv remotion/remotion.zip out/remotion-layer-remotion-v2.zip
