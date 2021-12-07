1f0ad99c5ce00be3e00fc3fbecea31d38544a94e
Jonny Burger <jonathanburger11@gmail.com>
2021-12-07 10:37:08 +0100

    new

--- make.sh
+++ make.sh
@@ -6,6 +6,6 @@ cd remotion && zip -9 --filesync --recurse-paths remotion.zip .
 cd ..
 
 mkdir -p out
-mv chromium/chromium.zip out/remotion-layer-chromium-v1.zip
-mv ffmpeg/ffmpeg.zip out/remotion-layer-ffmpeg-v1.zip
-mv remotion/remotion.zip out/remotion-layer-remotion-v1.zip
+mv chromium/chromium.zip out/remotion-layer-chromium-v3.zip
+mv ffmpeg/ffmpeg.zip out/remotion-layer-ffmpeg-v3.zip
+mv remotion/remotion.zip out/remotion-layer-remotion-v3.zip
