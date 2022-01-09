2d12fbff60dac2b67ef5a455f81a4f32a4865836
Jonny Burger <jonathanburger11@gmail.com>
2022-01-09 19:23:27 +0100

    v4 chrome 97

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-regions=( 'eu-central-1' 'eu-west-1' 'eu-west-2' 'us-east-1' 'us-east-2' 'us-west-2' 'ap-south-1' 'ap-southeast-1' 'ap-southeast-2' 'ap-northeast-1' )
+regions=( 'eu-central-1')
 for region in "${regions[@]}"
 do : 
     echo $region
