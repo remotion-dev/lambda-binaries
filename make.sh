4a3e7a39e522849c76aefb7269cceadfb72a4b1c
Jonny Burger <jonathanburger11@gmail.com>
2022-06-07 14:34:17 +0200

    update

--- make.sh
+++ make.sh
@@ -6,18 +6,18 @@ cd ffmpeg && zip -9 --filesync --recurse-paths ffmpeg.zip .
 cd ..
 cd ffmpeg-x64 && zip -9 --filesync --recurse-paths ffmpeg-x64.zip .
 cd ..
-cd remotion && zip -9 --filesync --recurse-paths remotion.zip .
+cd fonts && zip -9 --filesync --recurse-paths fonts.zip .
 cd ..
-cd remotion-x64 && zip -9 --filesync --recurse-paths remotion-x64.zip .
+cd fonts-x64 && zip -9 --filesync --recurse-paths fonts-x64.zip .
 cd ..
 
 mkdir -p out
 
-mv chromium/chromium.zip out/remotion-layer-chromium-v5-arm64.zip
-mv chromium-x64/chromium-x64.zip out/remotion-layer-chromium-v5-x86_64.zip
+mv chromium/chromium.zip out/remotion-layer-chromium-v6-arm64.zip
+mv chromium-x64/chromium-x64.zip out/remotion-layer-chromium-v6-x86_64.zip
 
-mv ffmpeg/ffmpeg.zip out/remotion-layer-ffmpeg-v5-arm64.zip
-mv ffmpeg-x64/ffmpeg-x64.zip out/remotion-layer-ffmpeg-v5-x86_64.zip
+mv ffmpeg/ffmpeg.zip out/remotion-layer-ffmpeg-v6-arm64.zip
+mv ffmpeg-x64/ffmpeg-x64.zip out/remotion-layer-ffmpeg-v6-x86_64.zip
 
-mv remotion/remotion.zip out/remotion-layer-remotion-v5-arm64.zip
-mv remotion-x64/remotion-x64.zip out/remotion-layer-remotion-v5-x86_64.zip
+mv fonts/fonts.zip out/remotion-layer-fonts-v6-arm64.zip
+mv fonts-x64/fonts-x64.zip out/remotion-layer-fonts-v6-x86_64.zip
--- size.sh
+++ size.sh
@@ -1,7 +1,7 @@
 du -sh chromium
 du -sh ffmpeg
-du -sh remotion
+du -sh fonts
 echo "----"
 du -sh chromium-x64
 du -sh ffmpeg-x64
-du -sh remotion-x64
+du -sh fonts-x64
