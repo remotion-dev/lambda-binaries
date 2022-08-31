4c40bbaa49b1faeb1bdbbd7fe604ffdeafe30b1f
Jonny Burger <jonathanburger11@gmail.com>
2022-08-31 11:29:42 +0200

    v8

--- upload.sh
+++ upload.sh
@@ -2,13 +2,13 @@ regions=( 'us-east-1' 'eu-central-1' 'eu-west-1' 'eu-west-2' 'us-east-2' 'us-wes
 for region in "${regions[@]}"
 do : 
     echo $region
-    aws s3 cp --region=$region out/remotion-layer-chromium-v7-arm64.zip s3://remotionlambda-binaries-$region/remotion-layer-chromium-v7-arm64.zip
-    aws s3 cp --region=$region out/remotion-layer-chromium-v7-x86_64.zip s3://remotionlambda-binaries-$region/remotion-layer-chromium-v7-x86_64.zip
+    aws s3 cp --region=$region out/remotion-layer-chromium-v8-arm64.zip s3://remotionlambda-binaries-$region/remotion-layer-chromium-v8-arm64.zip
+    aws s3 cp --region=$region out/remotion-layer-chromium-v8-x86_64.zip s3://remotionlambda-binaries-$region/remotion-layer-chromium-v8-x86_64.zip
 
-    aws s3 cp --region=$region out/remotion-layer-ffmpeg-v7-arm64.zip s3://remotionlambda-binaries-$region/remotion-layer-ffmpeg-v7-arm64.zip
-    aws s3 cp --region=$region out/remotion-layer-ffmpeg-v7-x86_64.zip s3://remotionlambda-binaries-$region/remotion-layer-ffmpeg-v7-x86_64.zip
+    aws s3 cp --region=$region out/remotion-layer-ffmpeg-v8-arm64.zip s3://remotionlambda-binaries-$region/remotion-layer-ffmpeg-v8-arm64.zip
+    aws s3 cp --region=$region out/remotion-layer-ffmpeg-v8-x86_64.zip s3://remotionlambda-binaries-$region/remotion-layer-ffmpeg-v8-x86_64.zip
     
-    aws s3 cp --region=$region out/remotion-layer-fonts-v7-arm64.zip s3://remotionlambda-binaries-$region/remotion-layer-fonts-v7-arm64.zip
-    aws s3 cp --region=$region out/remotion-layer-fonts-v7-x86_64.zip s3://remotionlambda-binaries-$region/remotion-layer-fonts-v7-x86_64.zip
+    aws s3 cp --region=$region out/remotion-layer-fonts-v8-arm64.zip s3://remotionlambda-binaries-$region/remotion-layer-fonts-v8-arm64.zip
+    aws s3 cp --region=$region out/remotion-layer-fonts-v8-x86_64.zip s3://remotionlambda-binaries-$region/remotion-layer-fonts-v8-x86_64.zip
 done
 
