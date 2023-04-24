fb36f25b3b1ec4f0a8f3e55e7956d26b4c669da3
Jonny Burger <jonathanburger11@gmail.com>
2023-04-24 11:35:17 +0200

    x64

--- make.sh
+++ make.sh
@@ -1,16 +1,9 @@
 cd chromium && zip -9 --filesync --recurse-paths chromium.zip .
 cd ..
-cd chromium-x64 && zip -9 --filesync --recurse-paths chromium-x64.zip .
-cd ..
 cd fonts && zip -9 --filesync --recurse-paths fonts.zip .
-cd ..
-cd fonts-x64 && zip -9 --filesync --recurse-paths fonts-x64.zip .
 cd ..
 
 mkdir -p out
 
 mv chromium/chromium.zip out/remotion-layer-chromium-v10-arm64.zip
-mv chromium-x64/chromium-x64.zip out/remotion-layer-chromium-v10-x86_64.zip
-
 mv fonts/fonts.zip out/remotion-layer-fonts-v10-arm64.zip
-mv fonts-x64/fonts-x64.zip out/remotion-layer-fonts-v10-x86_64.zip
--- size.sh
+++ size.sh
@@ -1,5 +1,2 @@
 du -sh chromium
 du -sh fonts
-echo "----"
-du -sh chromium-x64
-du -sh fonts-x64
